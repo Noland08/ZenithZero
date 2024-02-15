e213959dfa7df2f4c671169e476753144845a870
Noland08 <144099927+Noland08@users.noreply.github.com>
2024-02-15 18:57:51 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+const logo = document.querySelector(".logo");
+const attatchment = document.querySelector(".attatchment");
+const gif = document.querySelector(".gif");
+const ACsign = document.querySelector(".ACsign");
+
+ACsign.addEventListener("click", () => {
+    // attatchment.innerHTML="awww ur Gay ";
+});
